12df2964a3c10e3dede7602ebf1fcac07ef5c7b9
Chenze_Fan <chf007@ucsd.edu>
2024-07-03 03:46:28 +0000

    complete train file

--- train_caltech.py
+++ train_caltech.py
@@ -38,6 +38,7 @@ from models.imagebind_model import ModalityType, load_module, save_module
 from models.events import EventModel
 from train_events import ImageBindTrain
 from datasets.eclipdatasets.simple_tokenizer import tokenize
+from datasets.eclipdatasets.event2img import Event2ImageDataset
 
 logging.basicConfig(level=logging.INFO, force=True)
 
@@ -45,12 +46,50 @@ logging.basicConfig(level=logging.INFO, force=True)
 LOG_ON_STEP = True
 LOG_ON_EPOCH = True
 
-def load_model_from_checkpoint(checkpoint_path='/tsukimi/datasets/Chiba/finetune_checkpoint/checkpoints/imagebind-epoch=20-val_loss=0.00.ckpt'):
+
+def resize_pad(frame, size=224):
+    """
+    resize a frame's longer side to 224, pad the shorter side to 224
+    """
+
+    # get shape
+    c, h, w = frame.shape
+
+    # get longer side
+    longer_side = max(h, w)
+
+    # calculate ratio
+    ratio = size / longer_side
+
+    # resize with transform
+    resize_transform = transforms.Resize((int(h * ratio), int(w * ratio)))
+    frame = resize_transform(frame)
+
+    # get new shape
+    c, h, w = frame.shape
+
+    # calculate padding needed to reach size for both dimensions
+    pad_height = (size - h) if h < size else 0
+    pad_width = (size - w) if w < size else 0
+
+    # calculate padding for each side to center the image
+    pad_top = pad_height // 2
+    pad_bottom = pad_height - pad_top
+    pad_left = pad_width // 2
+    pad_right = pad_width - pad_left
+
+    # apply padding
+    padding_transform = transforms.Pad(padding=(pad_left, pad_top, pad_right, pad_bottom), fill=0, padding_mode='constant')
+    frame = padding_transform(frame)
+
+    return frame
+
+def load_model_from_checkpoint(checkpoint_path='/tsukimi/datasets/Chiba/finetune_checkpoint/checkpoints/imagebind-epoch=20-val_loss=0.00.ckpt',device=None):
     """
     Load a model from a checkpoint.
     """
     imgtrain=ImageBindTrain.load_from_checkpoint(checkpoint_path,map_location=torch.device('cpu'))
-    model = imgtrain.model.to(torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'))
+    model = imgtrain.model.to(device)
     return model
 
 class CaltechTrain(L.LightningModule):
@@ -60,7 +99,7 @@ class CaltechTrain(L.LightningModule):
         super().__init__()
         self.save_hyperparameters()
         
-        self.model=load_model_from_checkpoint(checkpoint_path)
+        self.model=load_model_from_checkpoint(checkpoint_path,device=self.device)
         self.model.eval()
         self.class_names=class_names
         self.prompt=prompt
@@ -162,16 +201,16 @@ class CaltechTrain(L.LightningModule):
         labels = data_dict['label'] # [B]
         logits = out_dict['logits']  # [B, n_classes]
         probs = out_dict['probs']  # [B, n_classes]
-        loss_dict = {}
-        if self.use_logits_loss:
-            loss_dict['ce_loss'] = F.cross_entropy(logits, labels)
-        if self.use_probs_loss:
-            probs = probs + 1e-6  # avoid nan
-            loss_dict['ce_loss'] = F.nll_loss(probs.log(), labels)
+        probs_acc = (probs.argmax(dim=-1) == labels).float().mean()
         
+        loss_dict = {}
+        loss_dict['ce_loss'] = F.cross_entropy(logits, labels)
+        loss_dict['probs_acc'] = probs_acc
+        
+        self.log("train_loss", loss_dict['ce_loss'], on_step=LOG_ON_STEP, on_epoch=LOG_ON_EPOCH,prog_bar=True)
+        self.log("train_acc", probs_acc, on_step=LOG_ON_STEP, on_epoch=LOG_ON_EPOCH,prog_bar=True)
         return loss_dict
     
-    @torch.no_grad()
     def calc_eval_loss(self, data_dict, out_dict):
         """Loss computation in eval."""
         loss_dict = self.calc_train_loss(data_dict, out_dict)
@@ -186,8 +225,94 @@ class CaltechTrain(L.LightningModule):
         logits = out_dict['logits']  # [B, n_classes]
         logits_acc = (logits.argmax(dim=-1) == labels).float().mean()
         loss_dict['logits_acc'] = logits_acc
+        
+        self.log("val_loss", loss_dict['ce_loss'], on_step=LOG_ON_STEP, on_epoch=LOG_ON_EPOCH,prog_bar=True)
+        self.log("val_acc", probs_acc, on_step=LOG_ON_STEP, on_epoch=LOG_ON_EPOCH,prog_bar=True)
         return loss_dict
     
+    def training_step(self, batch, batch_idx):
+        out_dict = self(batch)
+        loss_dict = self.calc_train_loss(batch, out_dict)
+        return loss_dict['ce_loss']
+    
+    def validation_step(self, batch, batch_idx):
+        out_dict = self(batch)
+        loss_dict = self.calc_eval_loss(batch, out_dict)
+        return loss_dict['ce_loss']
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train the ImageBind model classifier with PyTorch Lightning")
+    parser.add_argument("--seed", type=int, default=43, help="Random seed for reproducibility")
+    parser.add_argument("--device", type=str, default="cpu", help="Device to use for training ('cpu' or 'cuda')")
+    parser.add_argument("--datasets_dir", type=str, default="./.datasets",
+                        help="Directory containing the datasets")
+    parser.add_argument("--datasets", type=str, nargs="+", default="caltech", choices=["caltech"],
+                        help="Datasets to use for training and validation")
+    parser.add_argument("--loggers", type=str, nargs="+", choices=["tensorboard", "wandb", "comet", "mlflow"],
+                        help="Loggers to use for logging")
+    parser.add_argument("--loggers_dir", type=str, default="./.logs", help="Directory to save the logs")
+    parser.add_argument("--max_epochs", type=int, default=500, help="Maximum number of epochs to train")
+    parser.add_argument("--batch_size", type=int, default=12, help="Batch size for training and validation")
+    parser.add_argument("--lr", type=float, default=5e-6, help="Learning rate")
+    parser.add_argument("--weight_decay", type=float, default=1e-4, help="Weight decay")
+    parser.add_argument("--momentum_betas", nargs=2, type=float, default=[0.9, 0.95],
+                        help="Momentum beta 1 and 2 for Adam optimizer")
+    parser.add_argument("--temperature", type=float, default=0.07, help="Temperature parameter for InfoNCE loss")
+    parser.add_argument('--N', type=int, default=2000)
+    parser.add_argument("--num_workers", type=int, default=0, help="Number of workers for data loading")
+    parser.add_argument("--checkpoint_path", type=str, default=None,
+                        help="Path to checkpoint to load and continue training")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    
+    wandb_logger = pl_loggers.WandbLogger(
+                project="imagebind_finetune")
+    seed_everything(args.seed, workers=True)
+    torch.backends.cudnn.determinstic = True
+    device_name = args.device  # "cuda:0" if torch.cuda.is_available() else "cpu"
+    device = torch.device(device_name)
+    
+    data_transform=frame_normalize = transforms.Compose([
+                transforms.ToTensor(),
+                resize_pad])
+    quantize_args=dict(
+            max_imgs=2,
+            split_method='event_count',
+            convert_method='event_histogram',
+            N=20000,
+            grayscale=False,
+            count_non_zero=False,  # hotpixel statistics
+            background_mask=True,  # apply white background via alpha-masking
+        )
+    
+    if args.datasets=="caltech":
+        from datasets.eclipdatasets.caltech import NCaltech101,NEW_CNAMES
+        
+        train_dataset=NCaltech101(os.path.join(args.datasets_dir,'training'), augmentation=True, new_cnames=NEW_CNAMES)
+        class_names=train_dataset.classes
+        train_dataset=Event2ImageDataset(transforms=data_transform, event_dataset=train_dataset,quantize_args=quantize_args,augment=False,tta=False)
+        
+        val_dataset=NCaltech101(os.path.join(args.datasets_dir,'testing'), augmentation=False, new_cnames=NEW_CNAMES)
+        val_dataset=Event2ImageDataset(transforms=data_transform, event_dataset=val_dataset,quantize_args=quantize_args,augment=False,tta=False)
+    
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
+    
+    model=CaltechTrain(max_epochs=args.max_epochs, batch_size=args.batch_size, lr=args.lr,
+                           weight_decay=args.weight_decay, momentum_betas=args.momentum_betas,
+                           temperature=args.temperature,class_names=class_names,checkpoint_path='/tsukimi/datasets/Chiba/finetune_checkpoint/checkpoints/imagebind-epoch=20-val_loss=0.00.ckpt')
+    checkpointing = {"enable_checkpointing": True,
+                         "callbacks": [ModelCheckpoint(monitor="val_acc", dirpath=args.full_model_checkpoint_dir,
+                                                        filename="imagebind-{epoch:02d}-{val_loss:.2f}",
+                                                        save_last=True, mode="min")]}
+    
+    
+    
+
+
     
     
         
