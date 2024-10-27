aa91a78ec2b837078e0df45ce5c6294d0ea23288
j8chiu <j8qiu@ucsd.edu>
2024-10-27 00:13:37 +0000

    d

--- models/imagebind_model.py
+++ models/imagebind_model.py
@@ -206,7 +206,7 @@ class ImageBindModel(nn.Module):
         )
         
         self.autoencoder = AutoEncoder(in_dim=6, out_dim=3, relu=False)
-        self.autoencoder = load_and_freeze_model(self.autoencoder, '/eastdata/multi_percep_epoch47.ckpt')
+        self.autoencoder = load_and_freeze_model(self.autoencoder, '/eastdata/multi_percep_epoch99.ckpt')
         self.rgb_like_normalize = transforms.Compose([
             ## this is for the same method when saving the png
                 batch_min_max_normalize,
