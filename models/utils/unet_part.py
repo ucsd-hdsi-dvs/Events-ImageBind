60e2d77a7a37a7ccc553848d028d98b422290fb3
j8chiu <j8qiu@ucsd.edu>
2024-11-10 05:42:33 +0000

    d

--- models/utils/unet_part.py
+++ models/utils/unet_part.py
@@ -45,16 +45,16 @@ class Up(nn.Module):
     def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
 
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            #!TODO: add this to run
-            torch.use_deterministic_algorithms(False)
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            torch.use_deterministic_algorithms(True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
+        # # if bilinear, use the normal convolutions to reduce the number of channels
+        # if bilinear:
+        #     #!TODO: add this to run
+        #     torch.use_deterministic_algorithms(False)
+        #     self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        #     torch.use_deterministic_algorithms(True)
+        #     self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        # else:
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
